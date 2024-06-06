8dc7b78fef2c905b8d3d99b21472dcb6de3f8c64
MariaTrapero <mariajtm02@gmail.com>
2024-06-06 18:23:59 +0200

    Creation Template

---
+++ ProyectoIntermedio3/Source/ProyectoIntermedio3/ProyectoIntermedio3.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProyectoIntermedio3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProyectoIntermedio3, "ProyectoIntermedio3" );
+ 
